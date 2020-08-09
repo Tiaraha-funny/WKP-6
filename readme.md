df1354cd00077fd20d96c8f92b4415e4c992b7bf
unknown <jeannie.petah@gmail.com>
2020-08-09 16:45:16 +0300

    tried to finish the exercise but there are still problems which are not solved yet would be solved if have more time

--- readme.md
+++ readme.md
@@ -6,6 +6,36 @@ Yes, but who doesn't like food? 😄
 
 This time, we're going to build a food delivery app, where we can see the meal options, select them, and see the result in our cart.
 
+## 1- What I have done with this exercise
+
+- I drag the elements wich I need from the html
+- I created htmls for lists including spicy lists and vegetarian lists
+- I called the objects of an array inside the lists that I need
+- I created another html to add the order list in a menu
+- I created a Modal to tell that the clients have comfirmed their foods by clicking the comfirm button
+
+## 2- Difficulties
+
+I faced a lot of problems wich is not solved till now
+
+  - Getting the output into the order list
+  - Setting the progess of how often do the clients ordered the foods by using multiplication
+  - Increasing the ammount of what they ordered
+  - Giving the total ammount of the foods that is ordered
+## 3- Things needed to improve
+
+- Calling back an element from the html thatt is created in the js
+
+- I don't know how to use reduce with the array of object.
+
+### ****Message***
+I am so sorry that I didn't solve many things in this project. I really didn't find any way to solve them. I still need some push from anyone especially our teacher.
+**Thanks !!***
+
+#### *****Good thing****
+
+There are no errors in the console log in the browser
+
 ### Users must be able to :
 
 -   See a list of five meal options
---
+++ script.js
@@ -0,0 +1,177 @@
+//Copy paste the array
+
+const foods = [
+  {
+    id: 'ravitoto',
+    price: 5000,
+    title: 'Ravitoto',
+    spicy: true,
+    vegetarian: false,
+  },
+  {
+    id: 'pasta',
+    price: 4000,
+    title: 'Pasta',
+    spicy: true,
+    vegetarian: true,
+  },
+  {
+    id: 'burger',
+    price: 5000,
+    title: 'Burger',
+    spicy: false,
+    vegetarian: false,
+  },
+  {
+    id: 'rice',
+    price: 2000,
+    title: 'Rice and Leaves',
+    spicy: false,
+    vegetarian: true,
+  },
+  {
+    id: 'mofogasy',
+    price: 500,
+    title: 'Mofogasy',
+    spicy: false,
+    vegetarian: false,
+  },
+];
+console.log('work');
+// To generate any list of element (for example, the list of food), Map is really useful.
+
+//Drag all the elements needed
+
+const spicyFood = document.querySelector('.spicy');
+const vegetarianFood = document.querySelector('.vegetarian');
+const containsTheListOfFoods = document.querySelector('.container');
+const orderWhichAddedBtn = document.querySelector('.orderAdded');
+
+const submitModalBtn = document.querySelector('#submit');
+const outerModal = document.querySelector('.outer__modal');
+const innerModal = document.querySelector('.inner__modal');
+
+// Create HTML to hold the lists from the array of foods
+
+const mapSpicyFoods = e => {
+  const spicyHTML = foods.map(food => `
+    <div id="ravitoto">
+        <ul class="spicy__lists">
+          <li class="spicy__lists--items" id="${food.id}">
+            <div>${food.title}</li>
+            <div>${food.price}</li>
+            <button class="submit">Add</button>
+          </li>
+        </ul>
+      </div>
+    `).join(' ');
+  containsTheListOfFoods.innerHTML = spicyHTML;
+}
+
+// calling the function
+mapSpicyFoods();
+
+//create the html for the spicy food when the checkbox is checked
+
+const handleCheckboxSpicy = e => {
+  console.log('spicy');
+  if (spicyFood.checked) {
+    const filterSpicyFood = foods.filter(spicie => spicie.spicy === true);
+    console.log(filterSpicyFood);
+    const mappingSpicyHTML = filterSpicyFood.map(spicie =>
+      `<div id="${spicie.id}">
+        <ul class="spicy__lists">
+          <li class="spicy__lists--items" id="${spicie.id}">
+            <div>${spicie.title}</li>
+            <div>${spicie.price}</li>
+            <button class="submit">Add</button>
+          </li>
+        </ul>
+      </div>
+      `
+    ).join(' ');
+    containsTheListOfFoods.innerHTML = mappingSpicyHTML;
+  } else {
+    return mapSpicyFoods();
+  }
+}
+
+// create the html for the vegetarian food
+
+const handleCheckboxVegetarian = e => {
+  console.log('veges');
+  if (vegetarianFood.checked) {
+    const filterVegetarianFood = foods.filter(veges => veges.vegetarian === true);
+    console.log(filterVegetarianFood);
+    const mappingVegHTML = filterVegetarianFood.map(veget =>
+      `<div id="${veget.id}">
+        <ul class="vegetarian__lists">
+          <li class="vegetarian__lists--items" id="${veget.id}">
+            <div>${veget.title}</li>
+            <div>${veget.price}</li>
+            <button class="submit">Add</button>
+          </li>
+        </ul>
+      </div>
+      `
+    ).join(' ');
+    containsTheListOfFoods.innerHTML = mappingVegHTML;
+  } else {
+    return mapSpicyFoods();
+  }
+}
+
+// To add the order lists, create the html for that order
+
+const addBtn = document.querySelectorAll('.submit');
+
+const handleAddOrderLists = e => {
+  console.log('sumbmit');
+  if (e.target.matches('button.submit')) {
+      e.preventDefault();
+      const orderFoodsAddedHTML = `
+      <h2>Your Order</h2>
+      <ul class="order__lists">
+        <li class="order__lists--items" id="">
+          <div>Ravitoto</div>
+          <div>* 2</div>
+          <div>5000 Ariary</div>
+        </li>
+      </ul>
+    `
+    orderWhichAddedBtn.insertAdjacentHTML("beforeend", orderFoodsAddedHTML);
+    orderWhichAddedBtn.classList.add('flex');
+  }
+}
+
+
+// For the modal form
+
+//open the modal form
+
+const handleConfirmingBtn = e => {
+  outerModal.classList.add('open');
+  const modalHTML = `
+  <h2>Thank You !!</h2>
+  <p>Your order is confirmed !!<br>We will prepare your food order and deliver it to you when it is ready</p>
+  <p>The total amount is <strong>50000 Ariary</strong><p>
+  <button class="close">Close</button>
+  `
+  innerModal.innerHTML = modalHTML;
+}
+
+// handle the close button
+
+const handleCloseBtn = e => {
+  if (e.target.matches('button.close')) {
+    outerModal.classList.remove('open');
+  }
+}
+
+//To handle all the changes or clicks button
+
+window.addEventListener('click', handleAddOrderLists);
+vegetarianFood.addEventListener('change', handleCheckboxVegetarian);
+spicyFood.addEventListener('change', handleCheckboxSpicy);
+submitModalBtn.addEventListener('click', handleConfirmingBtn);
+document.addEventListener('click', handleCloseBtn);
